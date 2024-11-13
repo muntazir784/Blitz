406fe66ce01d4766dfe0157c82797df56d79b941
mdshyed <167856124+mdshyed@users.noreply.github.com>
2024-11-13 23:33:26 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { BrowserRouter as Router, Route, Routes,} from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import NoPage from "./pages/noPage/NoPage";
+import ProductInfo from "./pages/productInfo/ProductInfo";
+import ScrollTop from "./components/scrollTop/ScrollTop";
+import CartPage from "./pages/cart/CartPage";
+import AllProduct from "./pages/allProduct/AllProduct";
+import Signup from "./pages/registration/Signup";
+import Login from "./pages/registration/Login";
+import UserDashboard from "./pages/user/UserDashboard";
+import MyState from "./context/myState";
+import { Toaster } from "react-hot-toast";
+import { ProtectedRouteForUser } from "./protectedRoute/ProtectedRouteForUser";
+import CategoryPage from "./pages/category/CategoryPage";
+import Userprofileupdate from "./pages/user/Userprofileupdate";
+import Explore from './pages/explore/Explore';
+
+
+const App = () => {
+  return (
+    <MyState>
+      <Router>
+        <ScrollTop />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/*" element={<NoPage />} />
+          <Route path="/productinfo/:id" element={<ProductInfo />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/allproduct" element={<AllProduct />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/explore" element={<Explore/>} />
+          <Route path="/category/:categoryname" element={<CategoryPage />} /> 
+          <Route path="/user-dashboard" element={
+            <ProtectedRouteForUser>
+              <UserDashboard />
+            </ProtectedRouteForUser>
+          } />
+           <Route path="/Userprofileupdate" element={
+            <ProtectedRouteForUser>
+              <Userprofileupdate/>
+            </ProtectedRouteForUser>
+          } />
+        </Routes>
+        <Toaster />
+      </Router>
+    </MyState>
+  );
+}
+
+export default App;
---
+++ src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import Countdown from 'react-countdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Countdown renderer function
+const renderer = ({ days, hours, minutes, seconds }) => {
+  return (
+    <div className="bg-black bg-opacity-50 text-white py-2 px-4 text-center">
+      <div className="text-lg mb-1">Contest starts in: <span role="img" aria-label="clock">🕒</span></div>
+      <div className="text-xl font-bold">
+        {days}D : {hours}h : {minutes}m : {seconds}s
+      </div>
+    </div>
+  );
+};
+
+function HeroSection() {
+  // Set the target date for the countdown
+  const targetDate = new Date(Date.now() + 1000 * 60 * 60 * 38); // 1 day 14 hours from now
+
+  return (
+    <div className="w-full h-64 sm:h-80 md:h-96 lg:h-[650px] py-2 bg-[#2E2646]">
+      <Carousel>
+        <Carousel.Item>
+          <div className="relative">
+              <img
+                className="d-block w-full lg:h-[650px]"
+                 src="/slider_image_1.png"
+                alt="First slide"
+              />
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+        <div className="relative">
+            <img
+              className="d-block w-full lg:h-[650px]"
+              src="/slider_image_2.png"
+              alt="Second slide"
+            />
+            <div className="absolute bottom-20 md:bottom-40 lg:bottom-40 xl:bottom-40 left-1/2 transform -translate-x-1/2">
+              <Countdown date={targetDate} renderer={renderer} />
+            </div>
+            </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="relative">
+              <img
+                className="d-block w-full lg:h-[650px]"
+                 src="/slider_image_3.png"
+                alt="Third slide"
+              />
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
+
+export default HeroSection;
